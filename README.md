2b02404d4b8ffbafb8ebef14a221a6dbb8d67e29
Ryan Leite Albuquerque <ryan_albuquerque@apple.com>
2021-05-20 12:33:53 -0300

    fix: first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # testem-minimal-error-reporter
+---
+
+```js
+// testem.js
+const MinimalErrorReporter = require('testem-minimal-error-reporter');
+
+//...
+  reporter: MinimalErrorReporter
+```
---
+++ minimal-error-reporter.js
@@ -0,0 +1,20 @@
+import TapReporter from 'testem/lib/reporters/tap_reporter';
+import colors from 'colors/safe';
+
+class MinimalErrorReporter extends TapReporter {
+  display(prefix, result) {
+    if (!result.passed && !result.skipped && !result.todo) {
+      const line = result.name.replace(/Exam Partition .* -/, '').trim()
+      const output = '\n' + colors.red('[FAILED] ') + line + '\n';
+      this.out.write(output);
+    } else {
+      let output = '■';
+      if (result.passed) output = colors.green(output);
+      else if (result.skipped) output = colors.gray(output);
+      else if (result.todo) output = colors.yellow(output);
+      this.out.write(output);
+    }
+  }
+}
+
+module.exports = MinimalErrorReporter;
