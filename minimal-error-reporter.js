b17c1b4a16e1535241e8c779200c768eb3f6b3f5
Ryan Leite Albuquerque <ryan@cesar.org.br>
2022-10-25 20:34:31 -0300

    fix: add skipped tests in summary and save failures in a file

--- minimal-error-reporter.js
+++ minimal-error-reporter.js
@@ -1,6 +1,7 @@
 const TapReporter = require('testem/lib/reporters/tap_reporter');
 const colors = require('colors/safe');
 const displayutils = require('testem/lib/utils/displayutils');
+const fs = require("fs");
 
 class MinimalErrorReporter extends TapReporter {
   constructor(silent, out, config) {
@@ -8,13 +9,35 @@ class MinimalErrorReporter extends TapReporter {
     this.todo = 0;
     this.failed = 0;
     this.failedTests = [];
+    this.skippedTests = [];
+    this.startTime = new Date();
+    this.endTime = null;
+    this.out.write(`Starting at ${this.startTime.toISOString().substring(11, 19)}\n\n`);
+  }
+
+  finish() {
+    if (this.silent) {
+      return;
+    }
+    this.endTime = new Date();
+    this.out.write('\n' + this.summaryDisplay() + '\n');
   }
 
   display(prefix, result) {
     if (result.passed) this.out.write('.');
     else if (result.skipped) this.out.write('*');
-    else if (result.todo) this.out.write('-');
-    else {
+    else if (result.todo) {
+      this.out.write('-');
+
+      const skippedTest = {
+        id: this.id,
+        prefix,
+        result,
+      }
+
+      this.skippedTests.push(skippedTest);
+
+    } else {
       this.out.write('F');
 
       const failedTest = {
@@ -73,10 +96,28 @@ class MinimalErrorReporter extends TapReporter {
 }
 
 class MinimalErrorColoredReporter extends MinimalErrorReporter {
+  constructor(silent, out, config) {
+    super(silent, out, config);
+    fs.truncate('failures.ansi', 0, () => {});
+  }
   display(prefix, result) {
+    const line = result.name.replace(/Exam Partition .* -/, '').trim()
+    if (result.skipped) {
+      this.skippedTests.push(line);
+    }
+    
     if (!result.passed && !result.skipped && !result.todo) {
-      const line = result.name.replace(/Exam Partition .* -/, '').trim()
       this.failedTests.push(line)
+      fs.appendFile("failures.ansi", '\n\n\n' + colors.blue(line) + '\n\n', (err) => {
+        if (!err) { return ; }
+        console.error(err);
+      });
+
+      const resultString = displayutils.resultString(this.id, prefix, result, this.quietLogs, this.strictSpecCompliance)
+      fs.appendFile("failures.ansi", colors.brightRed(resultString), (err) => {
+        if (!err) { return ; }
+        console.error(err);
+      });
     }
     let output = '■';
     if (result.passed) this.out.write(colors.green(output));
@@ -102,13 +143,35 @@ class MinimalErrorColoredReporter extends MinimalErrorReporter {
     if (this.failedTests.length > 0) {
       lines.push('');
       lines.push('FAILED TESTS:');
+      lines.push('-------------');
       this.failedTests.sort();
       this.failedTests.forEach(function (failedTest) {
         const failedTestParts = failedTest.split(':');
-        lines.push('> ' + colors.blue(failedTestParts[0]) + ':' + colors.brightRed(failedTestParts[1]));
+        lines.push('» ' + colors.blue(failedTestParts[0]) + ':' + colors.brightRed(failedTestParts[1]));
       });
       lines.push('');
     }
+
+    if (this.skippedTests.length > 0) {
+      lines.push('');
+      lines.push('SKIPPED TESTS:');
+      lines.push('--------------');
+      this.skippedTests.sort();
+      this.skippedTests.forEach(function (skippedTest) {
+        const skippedTestParts = skippedTest.split(':');
+        lines.push('» ' + colors.blue(skippedTestParts[0]) + ':' + colors.gray(skippedTestParts[1]));
+      });
+      lines.push('');
+    }
+
+    var elapsed = this.endTime - this.startTime;
+    var date = new Date(0);
+    date.setMilliseconds(elapsed); // specify value for SECONDS here
+    var timeString = date.toISOString().substring(11, 19);
+    lines.push('');
+    lines.push(`Started at   : ${this.startTime.toISOString().substring(11, 19)}`);
+    lines.push(`Finishing at : ${this.endTime.toISOString().substring(11, 19)}`);
+    lines.push(colors.yellow(`Elapsed      : ${timeString}`));
 
     return lines.join('\n');
   }
